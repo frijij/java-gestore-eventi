a33b60986552236cbe239561a00a2c616219c483
francesca <francipeco36@gmail.com>
2023-09-05 12:39:12 +0200

    milestone 2: new event created

--- src/org/java/eventi/Main.java
+++ src/org/java/eventi/Main.java
@@ -1,5 +1,6 @@
 package org.java.eventi;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +8,17 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         //si chiede all’utente di inserire un nuovo evento con tutti i parametri.
+        System.out.println("---- BENVENUTO NEL NOSTRO SISTEMA DI PRENOTAZIONE DI EVENTI ----");
+        System.out.println("Inserisci un nuovo evento: ");
+        System.out.print("TITOLO ");
+        String titolo = scan.nextLine();
+        System.out.print("DATA ");
+        LocalDate data = LocalDate.parse(scan.nextLine());
+        System.out.print("POSTI DISPONIBILI ");
+        int postiDisponibili = Integer.parseInt(scan.nextLine());
 
+        Evento evento = new Evento( titolo, data, postiDisponibili);
+        System.out.println(evento.toString());
         //  chiedere all’utente se e quante prenotazioni vuole fare e provare ad effettuarle
 
         // Stampare a video il numero di posti prenotati e quelli disponibili
@@ -17,5 +28,7 @@ public class Main {
         // Provare ad effettuare le disdette
 
         // Stampare a video il numero di posti prenotati e quelli disponibili
+
+
     }
 }
